78c9abd15938e1f40d6858423774191c10a4224e
Matt Larson <matt.a.larson@gmail.com>
2017-05-09 11:13:57 -0500

    second commit

--- public/scripts/ngBasics.js
+++ public/scripts/ngBasics.js
@@ -1,1 +1,28 @@
 var myApp = angular.module('myApp', []);
+
+myApp.controller('InventoryController', function() { //convention is to cap every word
+  console.log('NG');
+  // variable global to this controller
+  var vm = this;
+  //array attached to controller (makes it available on the DOM)
+  vm.items = [];
+  //view model
+  vm.addItem = function() {
+    console.log('in addItem ng-click');
+    console.log(vm.nameIn, vm.descriptionIn);
+    //create object from user input
+    var newItem = {
+      name: vm.nameIn,
+      description: vm.descriptionIn
+    };
+    console.log('adding item:', newItem);
+    //push item into the array
+    vm.items.push(newItem);
+    console.log('items:', vm.items);
+    //empty inputs
+    vm.nameIn = '';
+    vm.descriptionIn = '';
+  }; //end addItem
+}); //end InventoryController
+
+//controller is part of app
--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
+//requires
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
 var index = require('./modules/index');
 
+//uses
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/')
+//base url hit
+app.use('/', index);
 
-var port =
+var port = 3000;
 
+//listen
 app.listen(port, function() {
-  console.log('listening');
+  console.log('listening on:', port);
 });
